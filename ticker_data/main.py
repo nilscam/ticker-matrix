5266d0124644ea9e235e3461367cd6d4e89e8fcf
Nils Cambreleng <nils.cambreleng@ramify.fr>
2022-10-22 20:45:39 +0200

    test with matrix connector

--- ticker_data/main.py
+++ ticker_data/main.py
@@ -9,8 +9,9 @@ import data_manager.main as data_manager
 # Main
 def main():
     data = data_manager.get_data()
-    display.message(data)
+    display.run_text(data)
+
 
 # Launcher
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
